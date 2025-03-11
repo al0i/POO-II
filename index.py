8a8bb278b47c3643fea4f99c97381241c8a7f4b6
Matheus William Joenck <117489072+al0i@users.noreply.github.com>
2025-03-10 21:51:39 -0300

    Window colision and main dome

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import pygame
 from models.Ball import Ball
+from models.Circle import Circle
+import random
 
 pygame.init()
 
@@ -10,17 +12,27 @@ pygame.display.set_caption("[PILOT] BINGO SIMULATOR")
 clock = pygame.time.Clock()
     
 b1 = Ball(window)
-
+c1 = Circle(window)
+contador = 0
 running = True
 while running:
     window.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
     b1.draw()
-    
     b1.move()
+    b1.colisao()
     
+    c1.draw()
+
+
+    contador+=1
+    if b1.x > c1.x+c1.radius or b1.x<c1.x+c1.radius:
+        print(contador,"colidiu!")
+
+
     pygame.display.flip()
     clock.tick(60)
 
--- models/Ball.py
+++ models/Ball.py
@@ -1,18 +1,35 @@
-import pygame
+import pygame, random
 
 class Ball:
     def __init__(self, window):
-        self.x = 1280/2
-        self.y = 720/2
-        self.velocity_x = 5
-        self.velocity_y = 5
-        self.radius = 15
-        self.color = (255,0,0) #branco
         self.window = window
+        self.radius = 25
+        self.x = random.randint(1,window.get_width()-1)
+        self.y = random.randint(1,window.get_height()-1)
+        self.velocity_x = random.choice([-5,5])
+        self.velocity_y = random.choice([-5,5])
+        self.color = (255,0,0)
 
     def move(self):
-        self.x += self.velocity_x
         self.y += self.velocity_y
+        self.x += self.velocity_x
+
+    def inverterY(self):
+        self.velocity_y *= -1
+    
+    def inverterX(self):
+        self.velocity_x *= -1
+
+    def colisao(self):
+        if self.y >= self.window.get_height():
+            self.inverterY()
+        if self.y <= 0:
+            self.inverterY()
+        if self.x >= self.window.get_width():
+            self.inverterX()
+        if self.x <= 0:
+            self.inverterX()
+        
 
     def draw(self):
         pygame.draw.circle(self.window,self.color,(self.x,self.y),self.radius)
