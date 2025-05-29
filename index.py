7b994e64ebcc0916c6a0a96eac6bbe64c60f91c8
Matheus William Joenck <117489072+al0i@users.noreply.github.com>
2025-05-28 23:21:59 -0300

    v1.1
    Player class with autoMove (follow the ball), collision corrections, ball style corrections...

--- index.py
+++ index.py
@@ -26,19 +26,38 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    paddle_group.update()
+    #paddle_group.update()
+    p2.update()
+    p1.update()
     paddle_group.draw(window)
     
-
-    ball_group.draw(window)
+    #ball_group.draw(window)
     ball_group.update()
 
     for b in ball_group:
         colided = pygame.sprite.spritecollide(b, paddle_group, False)
+        #if colided:
+        #    b.invertX()
         if colided:
-            b.invertX()
-        colided = False
+            # Detectar a direção do movimento da bola
+            if b.velocity_x > 0:  # bola indo para a direita
+                b.rect.right = colided[0].rect.left  # encosta a bola no lado esquerdo do paddle
+                b.invertX()
+            elif b.velocity_x < 0:  # bola indo para a esquerda
+                b.rect.left = colided[0].rect.right  # encosta a bola no lado direito do paddle
+                b.invertX()
 
+    #j1.autoMove()
+
+    print("P1: ", p1.pontos," - P2: ",p2.pontos)
+    if b1.screenColision() == 'left':
+        p2.pontos += 1
+        print("P1: ", p1.pontos," - P2: ",p2.pontos)
+    elif b1.screenColision() == 'right':
+        p1.pontos += 1
+        print("P1: ", p1.pontos," - P2: ",p2.pontos)
+
+    
     display_config()
 
 pygame.quit()
